6950457e6a680c652fe268329ad8cf68294f4073
Jafeth_256 <jafethvp25@gmail.com>
2024-11-28 15:26:11 -0600

    Creacion de sequencias para cada tabla

---
+++ Sequencias.sql
@@ -0,0 +1,44 @@
+-- Secuencia para la tabla usuarios
+CREATE SEQUENCE seq_usuarios
+START WITH 1
+INCREMENT BY 1;
+
+-- Secuencia para la tabla proveedores
+CREATE SEQUENCE seq_proveedores
+START WITH 1
+INCREMENT BY 1;
+
+-- Secuencia para la tabla mascota
+CREATE SEQUENCE seq_mascota
+START WITH 1
+INCREMENT BY 1;
+
+-- Secuencia para la tabla productos
+CREATE SEQUENCE seq_productos
+START WITH 1
+INCREMENT BY 1;
+
+-- Secuencia para la tabla servicios
+CREATE SEQUENCE seq_servicios
+START WITH 1
+INCREMENT BY 1;
+
+-- Secuencia para la tabla citas
+CREATE SEQUENCE seq_citas
+START WITH 1
+INCREMENT BY 1;
+
+-- Secuencia para la tabla historial_medico
+CREATE SEQUENCE seq_historial_medico
+START WITH 1
+INCREMENT BY 1;
+
+-- Secuencia para la tabla tratamientos
+CREATE SEQUENCE seq_tratamientos
+START WITH 1
+INCREMENT BY 1;
+
+-- Secuencia para la tabla ubicaciones
+CREATE SEQUENCE seq_ubicaciones
+START WITH 1
+INCREMENT BY 1;
